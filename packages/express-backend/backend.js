5ce7efda00970518b6b54a119b83c2c3c9d575f6
alonzoooooo <alonzoedwards2006@gmail.com>
2026-09-07 17:51:44 -0700

    get usah

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -39,6 +39,11 @@ const findUserByName = (name) => {
     return user.name === name;
   });
 };
+const findUserById = (id) => {
+  return users.users_list.find((user) => {
+    return user.id === id;
+  });
+};
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -53,7 +58,16 @@ app.get("/users", (req, res) => {
     res.send(users);
   }
 });
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const result = findUserById(id);
 
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
